08c1b205e8e775f7030d75c5e0711de9661e9bf5
Njeri Kieha <sylviakieha@gmail.com>
2026-03-12 11:59:44 +0300

    Custom loading and not found pages

---
+++ app/loading.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+
+function LoadingPage() {
+  const override = {
+    display: "block",
+    margin: "100px auto",
+  };
+
+  return (
+    <ClipLoader
+      color="#3b82f6"
+      loading={true}
+      cssOverride={override}
+      size={150}
+      aria-label="Loading Spinner"
+    />
+  );
+}
+
+export default LoadingPage;
